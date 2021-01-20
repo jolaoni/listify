45b0cdd285fb69269cd3b0c534b6cdcd09b70493
Jola Oni <jola.developer@emulug.com>
2021-01-20 16:40:52 -0600

    listify web api

---
+++ WebApi/Controllers/ListifyController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("listify")]
+    public class ListifyController : ControllerBase
+    {
+        private readonly ILogger<ListifyController> _logger;
+
+        public ListifyController(ILogger<ListifyController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public int Get(int begin, int end, int index)
+        {
+            if (begin >= end)
+                return 0;
+
+            var list = new ListifyCollection(begin, end);
+
+            return list[index];
+        }
+    }
+}
---
+++ WebApi/Util/ListifyCollection.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+public class ListifyCollection : IList<int>
+{
+    private IEnumerable<int> enumeration;
+
+    public ListifyCollection(int start, int end)
+    {
+        enumeration = Enumerable.Range(start, end);
+    }
+
+    public int this[int index]
+    {
+        get
+        {
+            return enumeration.ElementAt(index);
+        }
+        set
+        {
+            enumeration = enumeration.Select((x, i) =>
+            {
+                if (i == index)
+                    return value;
+                return x;
+            });
+        }
+    }
+
+    public int Count => enumeration.Count();
+
+    public bool IsReadOnly => false;
+
+    public void Add(int item)
+    {
+        enumeration = enumeration.Concat(new[] { item });
+    }
+
+    public void Clear()
+    {
+        enumeration = Enumerable.Empty<int>();
+    }
+
+    public bool Contains(int item)
+    {
+        return enumeration.Contains(item);
+    }
+
+    public void CopyTo(int[] array, int arrayIndex)
+    {
+        for (int i = arrayIndex; i < Count; i++)
+        {
+            array[i] = this[i];
+        }
+    }
+
+    public IEnumerator<int> GetEnumerator()
+    {
+        return enumeration.GetEnumerator();
+    }
+
+    public int IndexOf(int item)
+    {
+        var idx = enumeration.TakeWhile(x => x != item).Count();
+        if (enumeration.Any() && idx == Count && enumeration.Last() != item)
+        {
+            return -1;
+        }
+
+        return idx;
+    }
+
+    public void Insert(int index, int item)
+    {
+        this[index] = item;
+    }
+
+    public bool Remove(int item)
+    {
+        var countBefore = Count;
+        enumeration = enumeration.Except(new[] { item });
+        var countAfter = Count;
+
+        return countAfter < countBefore;
+    }
+
+    public void RemoveAt(int index)
+    {
+        enumeration = enumeration.Where((x, i) => i != index);
+    }
+
+    System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+    {
+        return enumeration.GetEnumerator();
+    }
+}
